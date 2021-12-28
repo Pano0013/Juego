389c712771de416cc9d00c89740a842a0f7cc5a4
Pano0013 <capoparodi@hotmail.com>
2021-12-28 18:01:37 -0300

    .

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 let enemigos = [];
+let personaje;
 
+function Personas(vida, ataque ,defensa){
+    this.vida = vida;
+    this.ataque = ataque;
+    this.defensa = defensa;
+}
+
+ function crearEnemigos(){
+     
+ }
